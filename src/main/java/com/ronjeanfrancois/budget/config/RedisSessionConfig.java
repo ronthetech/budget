15e3d82657a7fa7b024931f14f76f046ddb10b0d
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-02-11 23:38:45 -0500

    add redis session configuration

---
+++ src/main/java/com/ronjeanfrancois/budget/config/RedisSessionConfig.java
@@ -0,0 +1,6 @@
+package com.ronjeanfrancois.budget.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class RedisSessionConfig { }
